2eaca01341827ddc7a0a1136a06911e6fdfe3073
rgyani <rgyani48@gmail.com>
2025-08-28 19:19:42 +0530

    Add a project

---
+++ ___project_01_snake_water_gun_game.py
@@ -0,0 +1,33 @@
+import random
+
+computer = random.choice([-1, 0, 1])
+you = int(input("Enter your choice : "))
+reverse_dict = {-1:"snake", 0:"gun", 1:"water"}
+
+
+print(f" you chose {reverse_dict[you]} \ncomputer chose {reverse_dict[computer]} ")
+
+if(computer == you):
+    print("its draw")
+
+else:
+    if(computer == -1 and you == 0):
+        print("you win !")
+
+    elif(computer == -1 and you == 1):
+        print("you win !")
+
+    elif(computer == 0 and you == -1):
+        print("you lose !")
+
+    elif(computer == 1 and you == -1):
+        print("you win !")
+
+    elif(computer == 1 and you == 0):
+        print("you lose !")
+
+    elif(computer == 0 and you == 1):
+        print("you lose !")
+
+    else:
+        print("something went wrong")
